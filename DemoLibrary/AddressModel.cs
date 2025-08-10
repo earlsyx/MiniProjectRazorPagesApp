b6f8a2926e8a5d5fcdf5d482fa28a6a58c4f3a0d
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-08-10 17:09:18 +0800

    Create Library PersonModel and Person Address and an ISaveAddress interface with method save address - not impelemnted method

---
+++ DemoLibrary/AddressModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class AddressModel
+    {
+        public string  StreetAddress { get; set; }
+        public string City { get; set; }    
+        public string State { get; set; }    
+        public string ZipCode { get; set; }
+
+        //Lambda Expression, Here is the code I want you to return.
+        // a way of shortcuting things.
+        // lambda expression is basically just assumptions because you undesrtand what's going on
+        public string AddressDisplayValue => $"{StreetAddress}, {City}, {State} {ZipCode}";
+      
+
+    }
+}
---
+++ DemoLibrary/ISaveAddress.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public interface ISaveAddress
+    {
+        //interface, who ever implements this will have this method
+        void SaveAdress(AddressModel address); 
+    }
+}
